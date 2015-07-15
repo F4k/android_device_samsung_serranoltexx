cbd6344d6f4a28158a22e2dd02c56c91de46c34e
f4k <tusemociones@gmail.com>
2015-07-15 20:44:35 -0300

    Initial commit

---
+++ carbon.mk
@@ -0,0 +1,11 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltexx TARGET_DEVICE=serranolte
+
+PRODUCT_NAME := carbon_serranoltexx
